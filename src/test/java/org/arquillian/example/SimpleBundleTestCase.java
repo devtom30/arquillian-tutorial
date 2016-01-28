1581e6b8fd6f9d9cb56527501f80daff24e19edd
tom <tlornet@teclib.com>
2016-01-28 15:14:16 +0100

    Initial commit

---
+++ src/test/java/org/arquillian/example/SimpleBundleTestCase.java
@@ -0,0 +1,72 @@
+package org.arquillian.example;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.osgi.metadata.OSGiManifestBuilder;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(Arquillian.class)
+public class SimpleBundleTestCase {
+
+    @ArquillianResource
+    BundleContext context;
+
+    @Deployment
+    public static JavaArchive createdeployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "test.jar");
+        archive.setManifest(new Asset() {
+            public InputStream openStream() {
+                OSGiManifestBuilder builder = OSGiManifestBuilder.newInstance();
+                builder.addBundleSymbolicName(archive.getName());
+                builder.addBundleManifestVersion(2);
+                builder.addImportPackages(Bundle.class);
+                return builder.openStream();
+            }
+        });
+        return archive;
+    }
+
+    @Test
+    public void testBundleContextInjection() throws Exception {
+        assertNotNull("BundleContext injected", context);
+        System.out.println("BundleContext injected");
+        long bundleId = context.getBundle().getBundleId();
+        assertEquals("System Bundle ID", 0, bundleId);
+        System.out.println("System Bundle ID : " + bundleId);
+    }
+
+    @Test
+    public void testBundleInjection(@ArquillianResource Bundle bundle) throws Exception {
+        // Assert that the bundle is injected
+        assertNotNull("Bundle injected", bundle);
+
+        // Assert that the bundle is in state RESOLVED
+        // Note when the test bundle contains the test case it
+        // must be resolved already when this test method is called
+        assertEquals("Bundle RESOLVED", Bundle.RESOLVED, bundle.getState());
+
+        // Start the bundle
+        bundle.start();
+        assertEquals("Bundle ACTIVE", Bundle.ACTIVE, bundle.getState());
+
+        // Assert the bundle context
+        BundleContext context = bundle.getBundleContext();
+        assertNotNull("BundleContext available", context);
+
+        // Stop the bundle
+        bundle.stop();
+        assertEquals("Bundle RESOLVED", Bundle.RESOLVED, bundle.getState());
+    }
+}
